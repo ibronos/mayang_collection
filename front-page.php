3d88b69141070d06f03faecfca6362a2040e576d
ibronos <ibronossaidi@gmail.com>
2020-07-21 17:17:19 +0700

    update

--- front-page.php
+++ front-page.php
@@ -13,6 +13,11 @@
  */
 
 get_header();
+$menu_katalog = get_field('menu_katalog');
+$args = array(
+	'post_status' => 'published',
+);
+$products = wc_get_products( $args );
 ?>
     <!-- Subscribe-->
     <section class="page-section" id="subscribe">
@@ -40,60 +45,72 @@ get_header();
         </div>
     </section>
 
-
     <!-- Katalog Page -->
-    <section id="Katalog-page">
+    <section id="katalog-page">
     	<div class="container">
-	    	<div class="card-deck mb-3 text-center">
-		        <div class="card mb-4 shadow-sm">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Free</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>10 users included</li>
-		              <li>2 GB of storage</li>
-		              <li>Email support</li>
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-		          </div>
-		        </div>
-		        <div class="card mb-4 shadow-sm">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Pro</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>20 users included</li>
-		              <li>10 GB of storage</li>
-		              <li>Priority email support</li>
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-		          </div>
-		        </div>
-		        <div class="card mb-4 shadow-sm">
-		          <div class="card-header">
-		            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-		          </div>
-		          <div class="card-body">
-		            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-		            <ul class="list-unstyled mt-3 mb-4">
-		              <li>30 users included</li>
-		              <li>15 GB of storage</li>
-		              <li>Phone and email support</li>
-		              <li>Help center access</li>
-		            </ul>
-		            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-		          </div>
-		        </div>
-		      </div>
+    		<div class="row">
+    			<?php foreach ($menu_katalog as $key => $value) {?>
+    			<div class="col-md-6">
+				  <div class="card img-fluid" style="width:500px">
+				    <img class="card-img-top" src="<?= $value['gambar']['url']; ?>" alt="Card image" style="width:100%">
+				    <div class="card-img-overlay">
+					    <div class="d-flex align-items-start flex-column" style="height: 200px;">
+					    	<?php if($value['link']) { ?>
+							<div class="p-3" style="margin-top: 45%;">
+								<a href="<?= $value['link']; ?>" class="">
+									<span class="card-title"><?= $value['judul_link']; ?></span>
+								</a>
+							</div>
+							<?php } ?>
+						</div>    
+				    </div>
+				  </div>    				
+    			</div>
+	    		<?php } ?>
+    		</div>
     	</div>
-
     </section>
+
+    <section id="carousel-katalog">
+    	<div class="container">
+    		<div class="row">
+    			<?php foreach($products as $key => $value) { 
+    				$data = $value->get_data();
+    				$image_id = $value->get_image_id();
+    				$image_url = wp_get_attachment_url($image_id, 'small');
+    			?>
+    			<div class="col-md-3">
+    				<a href="">
+					  <div class="card img-fluid" style="width:500px">
+					    <img class="card-img-top" src="<?= $image_url; ?>" alt="Card image" style="width:100%">
+					    <div class="card-footer text-center">
+					    	<?php echo $value->get_title(); ?>		
+					    </div>
+					  </div> 
+					</a>
+    			</div>
+    		<?php } ?>
+    		</div>
+		</div>
+    </section>
+
+
+    <div id="portfolio">
+        <div class="container-fluid p-0">
+            <div class="row no-gutters">
+                <div class="col-lg-4 col-sm-6">
+                    <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg">
+                        <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="" />
+                        <div class="portfolio-box-caption">
+                            <div class="project-category text-white-50">Category</div>
+                            <div class="project-name">Project Name</div>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 <?php
 // get_sidebar();
